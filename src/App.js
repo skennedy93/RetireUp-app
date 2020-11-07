9e2c819ac7570943ec91b40c839ac874701fc39e
skennedy93 <sean47900@gmail.com>
2020-11-07 10:02:26 -0500

    Getting table data from data.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { Component } from "react";
 import './App.css';
+import data from './data.json'
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-
-
+      <table>
+        <tbody>
+        <tr>
+          <th>Year</th>
+          <th>Total Return</th>
+          <th>Cumulative Returns</th>
+        </tr>
+          {data.map(data => {
+                  return (
+                <tr key={data.year}>
+                    <td>{data.year}</td>
+                    <td>{data.totalReturn}</td>
+                </tr>
+                 );
+                })}
+        </tbody>
+      </table>
     </div>
   );
+}
 }
 
 export default App;
