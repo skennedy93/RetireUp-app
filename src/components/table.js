5bf3f7f591bcc81870ba1f24c90cc1b0ef4a80e3
skennedy93 <sean47900@gmail.com>
2020-11-11 14:30:19 -0500

    Added some styling for the table. Styling the slider is causing the slider to malfunction, trying to find a solution

--- src/components/table.js
+++ src/components/table.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components'
 
 export default function Table(props){
+    const Table = styled.table`
+    width: 75%; 
+    border-collapse: collapse; 
+    margin: auto;
+    `
+    const TableHeader = styled.th`
+    background: #333; 
+    color: white; 
+    font-weight: bold; 
+    `
+    const TableData = styled.td`
+    padding: 5px; 
+    border: 1px solid #ccc; 
+    `
 
 // setting the minimum and maximum numbers on the range by subtracting years from the selected numbers on the slider
 // this will allow me to use the slice method on those selected years to filter out anything that is not in between the 
@@ -11,22 +26,23 @@ export default function Table(props){
     let info = props.data.slice(min, max);
 
     return(
-        <table>
+        <Table>
             <tbody>
             <tr>
-                <th>Year</th>
-                <th>Total Return</th>
+                <TableHeader>Year</TableHeader>
+                <TableHeader>Total Return</TableHeader>
+                <TableHeader>Cumulative Returns</TableHeader>
             </tr>
                 {
                     info.map(data => (
                         <tr key={data.year}>
-                            <td>{data.year}</td>
-                            <td>{data.totalReturn}</td>
+                            <TableData>{data.year}</TableData>
+                            <TableData>{data.totalReturn}</TableData>
                         </tr>
                     ))
                 }
             </tbody>
-        </table>
+        </Table>
     );
 }
 
--- src/components/tableSlider.js
+++ src/components/tableSlider.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import styled from 'styled-components'
 
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
 export default function TableSlider(props) {
+
+  const Container = styled.table`
+  width: 75%; 
+  margin: auto;
+  `
+
   const min = props.min;
   const max = props.max;
 
   return (
       <div>
+        <Container>
         <Range
         min={min}
         max={max}
@@ -18,6 +26,7 @@ export default function TableSlider(props) {
         onChange={props.onChange}
         allowCross = {false}
         />
+        </Container>
       </div>
     );
   }
